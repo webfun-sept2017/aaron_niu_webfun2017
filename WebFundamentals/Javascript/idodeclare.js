8b79e22f4910d4cce6097b1adfb24578e0d6989d
aarondniu <aaronniu@hotmail.com>
2017-09-25 12:57:37 -0700

    idodeclare assignment added

---
+++ WebFundamentals/Javascript/idodeclare.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset:UTF-8>
+        <title>I Do Declare</title>
+        <script type="text/javascript">
+            console.long(document)
+            var Num1=77;
+        var Num2=66;
+        var Num3=1000;
+        var Num4=-55;
+        var String1="Batman";
+        var String2="Joker";
+        var String3="Gotham";
+        var Supermanisbetter=true;
+        var poisonivywins=false;
+        
+        console.log(Num1,Num2,Num3);
+        </script>
+    </head>
+    <body>
+        
+    </body>
+</html>
+
+
+
